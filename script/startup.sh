3dc2e3c1feae420d8f2997a2e0297e05382bbcbe
Abdelaziz BENJELLOUN <abenjelloun@kds.com>
2021-05-09 18:24:55 +0200

    deploying cron task for docker run command

--- script/startup.sh
+++ script/startup.sh
@@ -1,2 +1,6 @@
 dotnet appointment-checker.dll candilib
-dotnet appointment-checker.dll wedding
+#dotnet appointment-checker.dll wedding
+
+#crond -e
+#*/1 * * * * docker run -d appointmentchecker:latest > /dev/null
+#sudo service cron start
